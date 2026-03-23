af17ed880ae0484d602f6e44be9c88ff384c9e5c
Abdul Wasio <wasioabdul820@gmail.com>
2026-03-23 04:05:51 -0700

    Implement sum and average calculation for array
    Calculates the total sum and arithmetic mean of an integer array.

---
+++ problem01.java
@@ -0,0 +1,17 @@
+//Sum and Average: Calculate total sum and arithmetic mean of an integer array
+public class problem01 {
+    public static void main(String[] args) {
+        int sum = 0;
+        double arithmeticMean = 0;
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        for(int i=0; i<arr.length; i++) {
+            sum = sum + arr[i];
+        }
+
+        arithmeticMean = (double) sum/arr.length;
+        System.out.println("Sum is " + sum);
+        System.out.println("Arithmetic Mean is " + arithmeticMean);
+
+    }
+}
