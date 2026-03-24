5aef16f791a932f9d655e933c8fdd5f6b5fae4dc
Abdul Wasio <wasioabdul820@gmail.com>
2026-03-23 19:40:12 -0700

    Create problem03.java

---
+++ Array and Loops/problem03.java
@@ -0,0 +1,28 @@
+public class problem03 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        System.out.print("Original Array: ");
+        for(int i=0; i<arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        System.out.println();
+        
+        int start = 0;
+        int end = arr.length - 1;
+        
+        while(start<end) {
+            int temp = arr[end];
+            arr[end] = arr[start];
+            arr[start] = temp;
+            start++;
+            end--;
+        }
+
+        System.out.print("Reversed Array: ");
+        for(int i=0; i<arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
