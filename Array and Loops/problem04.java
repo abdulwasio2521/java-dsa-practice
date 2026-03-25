b0742b29d26d2e6ff6b490bf1b8f9e89b5eb31e1
Abdul Wasio <wasioabdul820@gmail.com>
2026-03-25 11:34:34 -0700

    Create problem04.java

---
+++ Array and Loops/problem04.java
@@ -0,0 +1,19 @@
+public class problem04 {
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 4, 5, 6, 7, 8, 9, 10, 44, 45};
+        int evenCount = 0;
+        int oddCount = 0;
+
+        for(int i=0; i<arr.length; i++) {
+            if(arr[i]%2==0) {
+                evenCount++;
+            }
+            else {
+                oddCount++;
+            }
+        }
+
+        System.out.println("Even Numbers are " + evenCount);
+        System.out.println("Odd Numbers are " + oddCount);
+    }
+}
