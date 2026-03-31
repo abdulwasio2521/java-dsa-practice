f110a429717b04208b0fc4338181c46c398bbe48
Abdul Wasio <wasioabdul820@gmail.com>
2026-03-31 11:43:24 -0700

    Update problem01.java

--- Array and Loops/problem01.java
+++ Array and Loops/problem01.java
@@ -5,7 +5,7 @@ public class problem01 {
         double arithmeticMean = 0;
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-        for(int i=0; i<10; i++) {
+        for(int i=0; i<arr.length; i++) {
             sum = sum + arr[i];
         }
 
